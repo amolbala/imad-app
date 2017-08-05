461f82519df8af7215afa9a739e35ab40c305db0
amolbala <abbunini@gmail.com>
2017-08-05 16:19:36 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,16 @@
 console.log('Loaded!');
+
+var img = document.getElementById("madi");
+var leftMargin = 0;
+var rightMargin = 0;
+function moveRight () {
+    leftMargin += 1;
+    img.style.marginLeft = leftMargin + "px";
+}
+function moveLeft () {
+    rightMargin +=1;
+    img.style.marginRight = rightMargin + "px";
+}
+img.onClick = function () {
+    var interval = setInterval(moveRight, 50);
+};
