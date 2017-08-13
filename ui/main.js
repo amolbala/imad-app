fe8f3b0de9a835c55714e082bc85d3b4a7e98bec
amolbala <abbunini@gmail.com>
2017-08-13 21:06:58 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -16,5 +16,25 @@ img.onclick = function () {
     var interval = setInterval(moveRight, 50);
 };
 
+// Counter code
 var counter = document.getElementById('counter');
-counter.onclick
+counter.onclick = function () {
+    // Create a request object
+    var request = new XMLHttpRequest();
+    
+    // Capture the response and store it in a variable
+    request.onreadystatechange = function () {
+        if (request.readyState === XMLHttpRequest.DONE) {
+            //Take some action
+            if (request.status === 200) {
+                var counter = request.responseText;
+                var span = document.getElementById('count');
+                span.innerHTML = counter.toString();
+            }
+        }
+        // Not done yet
+    };
+    // Make the request
+    request.open('GET', 'http://abbunini.imad.hasura-app.io/counter', true);
+    requset.send(null);
+};
