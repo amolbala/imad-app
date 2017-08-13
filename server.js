c1f04cf918fc24361094c33f33d76f359d551dcc
amolbala <abbunini@gmail.com>
2017-08-13 20:39:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -76,7 +76,7 @@ var counter = 0;
 app.get('/counter', function (req, res) {
     counter = counter + 1;
     res.send(counter.toString());
-})
+});
 
 app.get('/ui/madi.png', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'madi.png'));
